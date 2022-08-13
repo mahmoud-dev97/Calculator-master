858c94bc00de701c340c48673991e91c7e05bef1
wezaa <wezaa.mahmoud@gmail.com>
2022-08-13 11:18:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + "%";
+}
+function compute() {
+  var principal = document.getElementById("principal").value;
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    return document.getElementById("principal").focus();
+  }
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+  document.getElementById("result").innerHTML =
+    "If you deposit " +
+    "<span class='yellow'>" +
+    principal +
+    "</span>" +
+    ",<br>at an interest rate of " +
+    "<span class='yellow'>" +
+    rate +
+    "%" +
+    "</span>" +
+    "<br>You will receive an amount of " +
+    "<span class='yellow'>" +
+    interest +
+    "</span>";
+  ",<br>in the year " + "<span class='yellow'>" + year + "</span>";
+  ("<br>");
+}
